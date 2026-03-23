f2a1588862f41ea5fecef0336bd83eb0d89898d7
vfarylevych <vfarylevych@primocollect.com.ua>
2026-03-23 23:21:26 +0200

    phpstan

--- src/Facades/JWTAuth.php
+++ src/Facades/JWTAuth.php
@@ -1,14 +1,6 @@
 <?php
 
-/*
- * This file is part of jwt-auth.
- *
- * (c) 2014-2021 Sean Tymon <tymon148@gmail.com>
- * (c) 2021 PHP Open Source Saver
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+declare(strict_types=1);
 
 namespace ArtTiger\JWTAuth\Facades;
 
@@ -18,11 +10,9 @@ class JWTAuth extends Facade
 {
     /**
      * Get the registered name of the component.
-     *
-     * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
-        return 'tymon.jwt.auth';
+        return 'arttiger.jwt.auth';
     }
 }
